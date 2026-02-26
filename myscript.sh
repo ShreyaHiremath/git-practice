6fd2f60eab7dfb5983da9b76e1289ebb602fb548
ShreyaHiremath <91336539+ShreyaHiremath@users.noreply.github.com>
2026-02-26 12:16:56 +0530

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Pipeline script execution started"
+echo "Cureent Date:"
+date
+
+echo "Current User :"
+whoami
+
+echo "Listing files:"
+ls -la
+
+echo "Script completed Successfully "
